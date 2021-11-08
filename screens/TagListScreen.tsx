28b4c439d95fa493e16699b95f83907ce81a9e7b
kotahorii <khorii.micron@gmail.com>
2021-11-08 22:35:01 +0900

    tag form created

--- screens/TagListScreen.tsx
+++ screens/TagListScreen.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { VFC } from 'react';
 import tw from 'tailwind-rn';
-import { SafeAreaView, Text, Alert } from 'react-native';
+import { SafeAreaView, Text, Alert, TouchableOpacity } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout, selectUser } from '../slices/userSlice';
 import { auth } from '../firebaseConfig';
 import { IconButton } from '../components/IconButton';
-export const TagListScreen = () => {
+import { Title } from '../components/Title';
+import { RootStackParamList } from '../types/types';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+type Props = {
+  navigation: NativeStackNavigationProp<RootStackParamList, 'TagList'>;
+};
+
+export const TagListScreen: VFC<Props> = ({ navigation }) => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   const signOut = async () => {
@@ -18,6 +27,13 @@ export const TagListScreen = () => {
   };
   return (
     <SafeAreaView style={tw('flex-1 mt-5 items-center')}>
+      <Title first="Tag" last="List" />
+      <TouchableOpacity
+        style={tw('mt-2')}
+        onPress={() => navigation.navigate('CreateTag')}
+      >
+        <MaterialCommunityIcons name="tag-plus" size={40} color="#5f9ea0" />
+      </TouchableOpacity>
       <Text>{user.email}</Text>
       <IconButton name="logout" size={20} color="blue" onPress={signOut} />
     </SafeAreaView>
